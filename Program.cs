19b1ba040752e6dfaca95b74a93a8068f4e97ba3
jennap-itm <jennap@itmtrading.com>
2025-05-14 14:43:29 -0700

    Fix: Updated connection and app pool identity settings

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SpotPriceBridge.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
