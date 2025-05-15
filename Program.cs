d8a0cf781b4001dcaf4b6ec05bc21a7aa1d4632c
jennap-itm <jennap@itmtrading.com>
2025-05-15 09:18:12 -0700

    Fix Dockerfile, update project structure, and deploy container successfully

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using SpotPriceBridge.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
